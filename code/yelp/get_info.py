798bc57a4ee3d7bb14e17a23d080d01062553b40
Iuliana Pascu <ipascu@gmail.com>
2015-03-10 18:58:34 -0700

    pep8 edit

--- code/yelp/get_info.py
+++ code/yelp/get_info.py
@@ -1,5 +1,4 @@
-from pymongo import MongoClient
-from pymongo import errors
+from pymongo import MongoClient, errors
 from bs4 import BeautifulSoup
 import requests
 import sys
@@ -26,11 +25,13 @@ def get_npages(url):
         return int(pages)
     return 0
 
+
 def get_curr_rev_cnt(busi):
-    ''' 
-    how many reviews have we scraped for this business so far 
     '''
-    return len(busi.get('reviews',[]))
+    how many reviews have we scraped for this business so far
+    '''
+    return len(busi.get('reviews', []))
+
 
 def try_requests(url):
     '''
@@ -57,16 +58,15 @@ def try_requests(url):
 
 def get_reviews(busi, collection):
     logging.warning("getting reviews for " + busi['name'])
-           
-    if get_curr_rev_cnt(busi) < min(1600, busi.get('review_count', 0))
-        and busi.get('review_count', 0) > 0:
-        
+
+    if get_curr_rev_cnt(busi) < min(1600, busi.get('review_count', 0)
+                                    ) and busi.get('review_count', 0) > 0:
+
         rev = []
-	
-        collection.update({"id" : busi['id']}, { '$set' : { 'reviews': [] } })
+        collection.update({"id": busi['id']}, {'$set': {'reviews': []}})
         npages = get_npages(busi['url'])
-    
-        pagen = min(40,npages)
+
+        pagen = min(40, npages)
         for page in xrange(pagen):
 
             index = page * 40
@@ -78,46 +78,36 @@ def get_reviews(busi, collection):
 
             if len(text) > 0:
                 soup = BeautifulSoup(text, "html.parser")
-                rev.extend(soup.find_all('div', class_ = 'review'))
-            
+                rev.extend(soup.find_all('div', class_='review'))
             time.sleep(0.25)
         try:
-            collection.update({"id" : busi['id']}, { '$set' : { 'reviews': [ { 'html' : str(item) } for item in rev ]} })
+            collection.update({'id': busi['id']}, {'$set':
+                {'reviews': [{'html': str(item)} for item in rev]}})
         except:
             with open("failed.txt", "w") as failed:
                 for item in rev:
-                  failed.write("%s\n" % item)        
+                    failed.write("%s\n" % item)
 
-def parse_reviews(busi, collection):
-    print "getting stars for " + busi['name']
-
-    business = collection.find_one({"id" : busi['id'] })
-    for review in business['reviews']:
-        soup = BeautifulSoup(review['html'], "html.parser")
- 
-        if soup.find(itemprop = "reviewRating"):
-            review['rating'] = soup.find(itemprop = "ratingValue")['content']
-            #collection.save(business)
-            
 if __name__ == '__main__':
     count = 0
     # find the restaurants with no reviews scraped or an empty list of reviews
-    item_lst = list(coll.find({ "$or": [{ "reviews" : {"$exists" :0}}, {"reviews": {"$size":0}}]}).sort('_id'))
+    item_lst = list(coll.find({"$or": [{"reviews": {"$exists": 0}},
+        {"reviews": {"$size": 0}}]}).sort('_id'))
     print len(item_lst)
-   
+
     start = int(sys.argv[1])
     end = int(sys.argv[2])
-    name = "log_%d_%d.txt" % (start, end)	
+    name = "log_%d_%d.txt" % (start, end)
     logging.basicConfig(filename=name, level=logging.WARNING)
 
     sublst = item_lst[start:end]
     for busi in sublst:
-	time.sleep(1)
-	start = time.time()
+        time.sleep(1)
+        start = time.time()
         logging.warning('restaurant: %d ' % count)
         get_reviews(busi, coll)
         count += 1
-	end = time.time()
-	logging.warning("took %d seconds" % (end-start))
-    
+        end = time.time()
+        logging.warning("took %d seconds" % (end-start))
+
     logging.warning("DONE")
